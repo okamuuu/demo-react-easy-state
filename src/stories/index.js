4426cb9a7059086a5d28e8e7c8fc5e8cf153b0b9
okamuuu <okamuuu@gmail.com>
2018-08-02 21:05:42 +0900

    feature: add dnd

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,15 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
-import Counter from './Counter';
 import Game from '../components/Game';
 import Game2 from '../components/Game2';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Counter', module)
-  .add('counter1', () => <Counter />)
 
 storiesOf('Game', module)
   .add('game1', () => <Game />)
